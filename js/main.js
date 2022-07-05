3d876f604461c626a8cf53522aa2dc4dfee0e03b
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-07-05 19:33:17 +0300

    changes to header design

--- js/main.js
+++ js/main.js
@@ -5,81 +5,13 @@ $(document).ready(function(){
    $('.slider').slick({
       arrows: true,
       speed: 500,
-      variableWidth: true
+      variableWidth: true,
+      centerMode: true,
+      adaptiveHeight: true
     });
 })
 
-
-
 //    Slick slider end
-
-//    Popup form start
-
-// let bodyScrollDisableFlag = false;
-// let acceptionWindowFlag = false;
-
-// const formLink = document.querySelector(".portfolio__form-link");
-
-// const contactForm = document.querySelector(".popup-form");
-
-// const popupFormButton = document.querySelector(".popup-form__button");
-
-// function hideShowScroll(){
-//    if((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))){
-//       // true for mobile device
-//       // console.log("check")
-//       console.log("check")
-//       if(!bodyScrollDisableFlag){ //checking if scroll was already disabled
-//          document.body.style.overflow = 'hidden'; //scroll disable
-//          bodyScrollDisableFlag = true; //setting memory flag for returning scroll back in the future
-//       } else{
-//          document.body.style.overflow = 'auto';
-//             bodyScrollDisableFlag = false;
-//       }
-// } else{
-//    if(!bodyScrollDisableFlag){ //checking if scroll was already disabled
-//       document.body.style.overflow = 'hidden'; //scroll disable
-//       document.body.style.left = '-7px'; //removing jumping of the content due to scroll width being removed
-//       bodyScrollDisableFlag = true; //setting memory flag for returning scroll back in the future
-//    } else{
-//       document.body.style.overflow = 'auto';
-//          document.body.style.left = '0';
-//          bodyScrollDisableFlag = false;
-//    }
-// }
-// }
-
-// function showForm(){
-//    document.querySelector(".popup-form").classList.toggle("popup-form--active");
-//    setTimeout(()=>document.querySelector(".popup-form__body").classList.toggle("popup-form__body--active"), 10);
-//    //disabling scroll
-//    hideShowScroll();
-// }
-// function hideForm(event){
-//    if(!event.target.closest(".popup-form__body")){
-//       document.querySelector(".popup-form").classList.toggle("popup-form--active");
-//       document.querySelector(".popup-form__body").classList.toggle("popup-form__body--active");
-//       //returning scroll
-//       hideShowScroll();
-//       if(acceptionWindowFlag){
-//          document.querySelector(".popup-form__wrapper").classList.toggle("popup-form__wrapper--accepted");
-//          acceptionWindowFlag = false;
-//       }
-//    }
-// }
-
-// function showAccepted(event){
-//    event.preventDefault();
-//    document.querySelector(".popup-form__wrapper").classList.toggle("popup-form__wrapper--accepted");
-//    acceptionWindowFlag = true;
-// }
-
-// formLink.addEventListener("click", showForm);
-// contactForm.addEventListener("click", hideForm);
-// popupFormButton.addEventListener("click", showAccepted);
-
-//    Popup form end
-
 
 //    Burger menu show\hide start
 
