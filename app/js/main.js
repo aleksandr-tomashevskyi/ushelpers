1f02573f58e3cec6dd6d8b0f106a50dc0d8fedb0
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-07-06 17:42:03 +0300

    created nested list arrows and logic for them and lists

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       variableWidth: true,
       centerMode: true,
       adaptiveHeight: true
-    });
+   });
 })
 
 //    Slick slider end
@@ -26,7 +26,7 @@ function showHideBurgerMenu(event){
    } else{
       burgerMenuMemoryFlag = false;
    }
-   hideShowScroll();
+   // hideShowScroll();
 }
 
 menuIconBlock.addEventListener("click", showHideBurgerMenu)
@@ -35,13 +35,58 @@ menuIconBlock.addEventListener("click", showHideBurgerMenu)
 
 //    Header inner lists start
 
-function headerInnerList(event){
-   if(event.target.closest(".navigation__inner-list-trigger")){
-   document.querySelector(".navigation__inner-list").classList.toggle("navigation__inner-list--active");
-}
+let listActivationFlag = false;
+let navInnerElements;
+let navListTrigger;
+
+
+function showInnerList(el){
+   if (el.matches(".navigation__inner-list")){
+      el.classList.toggle("navigation__inner-list--active");
+      el.parentElement.classList.toggle("navigation__inner-list-trigger--active");
+   }
 }
 
-document.querySelector(".navigation__list").addEventListener("click", headerInnerList);
+function innerListClickCheck(event){
+   //checking if the click was on trigger list el
+   if(event.target.closest(".navigation__inner-list-trigger")){
+      //checking if it's already open
+      if(listActivationFlag){
+         //checking if clicked trigger is the same as already opened list
+         if(event.target.closest(".navigation__inner-list-trigger").firstChild.nextSibling.textContent == navListTrigger.firstChild.nextSibling.textContent){
+            //closing already opened list
+            navInnerElements = event.target.closest(".navigation__inner-list-trigger").children;
+            headerInnerList(navInnerElements);
+         } else{
+            //closing already opened list and opening new one
+            headerInnerList(navInnerElements);
+            navInnerElements = event.target.closest(".navigation__inner-list-trigger").children;
+            headerInnerList(navInnerElements);
+         }
+      } else{
+         //opening inner list
+         navInnerElements = event.target.closest(".navigation__inner-list-trigger").children;
+         headerInnerList(navInnerElements);
+      }
+      // remembering the item with opened list for future checks
+      navListTrigger = event.target.closest(".navigation__inner-list-trigger");
+   } else{
+      //closing already opened list after click elsewhere
+      if(listActivationFlag){
+         headerInnerList(navInnerElements);
+      }
+   }
+   
+}
+
+function headerInnerList(element){
+   Array.from(element).forEach(showInnerList);
+   listActivationFlag = !listActivationFlag; //toggle boolean
+}
+
+if((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))){
+   document.addEventListener("click", innerListClickCheck);
+}
 
 
 //    Header inner lists end
