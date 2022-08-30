c70868cc18dfb369f2e2f4177da4bc67cfa216ed
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-08-30 12:09:10 +0300

    created more correct default post template

--- single.php
+++ single.php
@@ -1,3 +1,21 @@
 <?php
-echo "test";
+
+   get_header();
+?>
+<main class="main">
+   <div class="container">
+<?php
+while(have_posts()){
+   the_post();
+   the_title();
+   the_content();
+}
+
+?>
+</div>
+</main>
+<?php 
+
+get_footer();
+
 ?>
