b6146f95ec5392fafab7da9add8af54bb9b87570
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-09-01 12:03:45 +0300

    fixed hiring main block

--- template-parts/main-block.php
+++ template-parts/main-block.php
@@ -1,7 +1,7 @@
 <?php get_template_part( 'template-parts/popup'); ?>
 <section class="main-block">
    <div class="container">
-      <div class="main-block__columns <?php if(is_child()) echo 'main-block__columns--solutions-children'?>">
+      <div class="main-block__columns <?php if(is_child()) echo 'main-block__columns--solutions-children'?> <?php echo 'main-block__columns--', str_replace(" ","-", strtolower(get_the_title()));?>">
          <div class="main-block__column main-block__column--solutions main-block__column--<?php echo str_replace(" ","-", strtolower(get_the_title()));?>">
             <div class="main-block__image-container">
                      <img class="main-block__image" src="<?php the_field('main-block__image');?>" alt="image">
