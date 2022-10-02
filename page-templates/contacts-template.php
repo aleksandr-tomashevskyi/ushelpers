5da2c307b20fd6bb669dce0058bfc66166597d12
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-02 22:01:28 +0300

    added link like func to contacts page with styling

--- page-templates/contacts-template.php
+++ page-templates/contacts-template.php
@@ -17,7 +17,7 @@ get_header();
             <div class="contacts__block-wrapper">
                <div class="contacts__text-container">
                   <h2 class="contacts__title">OUR CONTACTS</h2>
-                  <p class="contacts__description">Call or text us by:<br><?php the_field('contacts__address-field-1')?><br><?php the_field('contacts__address-field-2')?><br><?php the_field('contacts__address-field-3')?></p>
+                  <p class="contacts__description">Call or text us by:<br>WhatsApp: <a href="https://wa.me/<?php str_replace(' ', '', the_field('contacts__address-field-1'))?>" class="contacts__whattsup-link"><?php the_field('contacts__address-field-1')?></a><br>Mail: <a href="mailto:<?php the_field('contacts__address-field-2')?>" class="contacts__email-link"><?php the_field('contacts__address-field-2')?></a><br><?php the_field('contacts__address-field-3')?></p>
                </div>
             </div>
          </div>
