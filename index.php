2db2aaeed0d3789e0f85d6128884567e8f625cd8
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-06 11:51:42 +0300

    created styles for single post container

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ get_header();
       <?php 
       while(have_posts()){
       the_post(); ?>
-         <div class="post">
-            <div class="post__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
-            <div class="post__date-container"><?php echo get_the_date() ?></div>
-            <div class="post__title-container"><?php echo get_the_title() ?></div>
-            <div class="post__excerpt-container"><?php echo get_the_excerpt() ?></div>
-            <div class="post__read-more-button-container"></div>
+         <div class="post-blog">
+            <div class="post-blog__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
+            <div class="post-blog__date-container"><p class="post-blog__date"><?php echo get_the_date(); ?></p></div>
+            <div class="post-blog__title-container"><h3 class="post-blog__title"><?php echo get_the_title(); ?></h3></div>
+            <div class="post-blog__excerpt-container"><p class="post-blog__excerpt"><?php echo get_the_excerpt(); ?></p></div>
+            <div class="post-blog__read-more-link-container"><a href="<?php echo get_permalink(get_the_ID()); ?>" class="post-blog__read-more-link">Read more</a></div>
          </div> 
       <?php
 }
