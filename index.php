f625026f28e9042bb1168b8de4e2d39c065efd22
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-06 14:56:14 +0300

    started work on single page structure

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ get_header();
                the_post(); ?>
                   <div class="post-blog">
                      <div class="post-blog__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
-                     <div class="post-blog__date-container"><p class="post-blog__date"><?php echo get_the_date(); ?></p></div>
+                     <div class="post-blog__date-container"><p class="post-blog__date">Published on: <?php echo get_the_date(); ?></p></div>
                      <div class="post-blog__title-container"><h3 class="post-blog__title"><?php echo get_the_title(); ?></h3></div>
                      <div class="post-blog__excerpt-container"><p class="post-blog__excerpt"><?php echo get_the_excerpt(); ?></p></div>
                      <div class="post-blog__read-more-link-container"><a href="<?php echo get_permalink(get_the_ID()); ?>" class="post-blog__read-more-link">Read more</a></div>
--- single.php
+++ single.php
@@ -4,14 +4,17 @@
 ?>
 <main class="main">
    <div class="container">
-<?php
-while(have_posts()){
-   the_post();
-   the_title();
-   the_content();
-}
-
-?>
+      <div class="post-single">
+         <div class="post-single__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
+         <div class="post-single__wrapper">
+            <div class="post-single__content">
+               <div class="post-single__title-container"><h1 class="post-single__title"><?php echo get_the_title(); ?></h1></div>
+               <div class="post-single__date-container"><p class="post-single__date">Published on: <?php echo get_the_date(); ?></p></div>
+               <div class="post-single__post-content-container"><p class="post-single__post-content"><?php the_content(); ?></p></div>
+            </div>
+            <div class="post-single__highlights-container"></div>
+         </div>
+      </div>
 </div>
 </main>
 <?php 
