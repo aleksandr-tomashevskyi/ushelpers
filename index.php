f05ee426f7c33a5ac872cad59c6ac0037992648e
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-06 11:29:49 +0300

    created blog page core structure

--- index.php
+++ index.php
@@ -3,12 +3,24 @@ get_header();
 ?>
 
 <main class="main">
-<?php 
-while(have_posts()){
-   the_post();
-   the_content();
+   <div class="container">
+      <div class="posts-container">
+      <?php 
+      while(have_posts()){
+      the_post(); ?>
+         <div class="post">
+            <div class="post__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
+            <div class="post__date-container"><?php echo get_the_date() ?></div>
+            <div class="post__title-container"><?php echo get_the_title() ?></div>
+            <div class="post__excerpt-container"><?php echo get_the_excerpt() ?></div>
+            <div class="post__read-more-button-container"></div>
+         </div> 
+      <?php
 }
 ?>
+      </div>
+
+</div>
 </main>
 
 <?php
--- page.php
+++ page.php
@@ -7,43 +7,12 @@ get_header();
 <?php 
 while(have_posts()){
    the_post();
-   the_content();
+   ?><p><?php the_content();?>test test test</p><?php 
+   echo get_the_post_thumbnail();
 }
 ?>
-               <div class="popup-window__calendly-content">
-                  <!-- Calendly inline widget begin -->
-                  <div class="calendly-inline-widget" data-url="https://calendly.com/alehandro-tom/30min" style="min-width:320px;height:630px;"></div>
-                  <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-                  <!-- Calendly inline widget end -->
-               </div>
-               <div class="popup-window__quiz-content">
-                  <?php echo 'do_shortcode("[qsm quiz=2]")'; ?>
-
-                  
-<div class="container">
-   <?php echo do_shortcode('[fcs_initialize]') ?>
-</div>
 <?php echo get_the_ID(); ?>
-<div class="container">
-   <ul class="free-call-entries">
-      <?php 
-      $freeCallEntries = new WP_Query(array(
-         'post_type' => 'fcs_entries_data',
-         'posts_per_page' => -1
-      ));
-      while($freeCallEntries->have_posts()){
-         $freeCallEntries->the_post();?>
-         <li>
-            <input value='<?php echo esc_attr(get_the_title());?>'>
-            <textarea><?php echo esc_attr(get_the_content()); ?></textarea>
-         </li>
-         <?php
-      }
-      ?>
-   </ul>
-</div>
-<?php echo get_the_ID(); ?>
-<?php echo plugins_url('free-call-scheduler/fcs.js'), 'something'; ?>
+<p>Test</p>
 
 </main>
 
