8e33c74cdf5dd15dba9a892f8f90cb4ff356dff7
aleksandr-tomashevskyi <oleksandr.o.tomashevskyi@gmail.com>
2022-10-14 17:03:11 +0300

    added link func to post titles

--- index.php
+++ index.php
@@ -21,7 +21,11 @@ $no_posts_title = '<h1 class="blog-container__no-posts-title">Sorry, there are n
                      <div class="post-blog">
                         <div class="post-blog__thumbnail-container"><img src="<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); echo $image[0];?>" /></div>
                         <div class="post-blog__date-container"><p class="post-blog__date">Published on: <?php echo get_the_date(); ?></p></div>
-                        <div class="post-blog__title-container"><h3 class="post-blog__title"><?php echo get_the_title(); ?></h3></div>
+                        <div class="post-blog__title-container">
+                           <a href="<?php echo get_permalink(get_the_ID()); ?>" class="post-blog__title-link">
+                              <h3 class="post-blog__title"><?php echo get_the_title(); ?></h3>
+                           </a>
+                        </div>
                         <div class="post-blog__excerpt-container"><p class="post-blog__excerpt"><?php echo get_the_excerpt(); ?></p></div>
                         <div class="post-blog__read-more-link-container"><a href="<?php echo get_permalink(get_the_ID()); ?>" class="post-blog__read-more-link">Read more</a></div>
                      </div>
